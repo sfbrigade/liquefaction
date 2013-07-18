c45c32b38dbcaf6b72ecdb021fef8a012b120478
rcackerman <rebecca@cfa>
2013-07-17 19:45:13 -0700

    Adding notes in comments.

--- checker.js
+++ checker.js
@@ -1,17 +1,23 @@
 // Quick and dirty function to check against JSON file
 
+//pull in info from form
+
+
+//scan
 function checkLiquifactionStatus( obj, lst ) {
 	if ( obj in lst ) {
 		return true;
 	} else {
 		return false;
 	}
-};
+}
 
 var userBlockLot = form.elements["block"].value + "," + form.elements["lot"].value;
 console.log(userBlockLot);
 
-var liquifactionList = $.getJSON('./listings.json', function() { //this is not referencing a real file
+var liquifactionList = $.getJSON('./data.json', function() { //this is not referencing a real file
 	console.log("success");
 })
-.done( checkLiquifactionStatus ( userBlockLot, liquifactionList ) );
+.done( console.log(checkLiquifactionStatus ( userBlockLot, liquifactionList )) );
+
+//return
