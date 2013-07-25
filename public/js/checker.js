fd4b17ec210b4d3fc359cd89bfdc55b19ac75f2e
Sean Knox <knoxville@gmail.com>
2013-07-24 19:39:53 -0700

    results div

--- public/js/checker.js
+++ public/js/checker.js
@@ -3,16 +3,16 @@
 //scan
 function checkLiquifactionStatus() {
 
-	var userBlockLot = form.elements["block"].value + "," + form.elements["lot"].value;
-	console.log(userBlockLot);
+  var userBlockLot = form.elements["block"].value + "," + form.elements["lot"].value;
+  console.log(userBlockLot);
 
-	$.getJSON('./data.json', function(data) {
-			console.log("success");
-		}).done(function( userBlockLot, json ) {
-			if ( userBlockLot in json ) {
-				return true;
-			} else {
-				return false;
-			}
-		});
+  $.getJSON('./data.json', function(data) {
+      console.log("success");
+    }).done(function( userBlockLot, json ) {
+      if ( userBlockLot in json ) {
+        return true;
+      } else {
+        return false;
+      }
+    });
 }
