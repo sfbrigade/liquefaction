58b8ddd0306e433dc1e387769345d3c120520d57
rcackerman <rebecca@cfa>
2014-01-07 11:54:15 -0500

    Fixing leading 0s bug.

--- checker.js
+++ checker.js
@@ -2,23 +2,22 @@ function checkliquefactionStatus() {
   resetFound();
 
   $.getJSON('./data.json', function(bl_pairs) {
-    console.log("loaded JSON");
     var input_block = $('#inputBlock').val();
-    var input_lot = $('#inputLot').val().match(/[^0].*/);
-    console.log(input_lot[0]);
-    // $(".blockNum").text(input_block); // Adds the input to the result message.
-    // $(".lotNum").text(input_lot); // Adds the input to the result message.
+    var input_lot = $('#inputLot').val();
+    $(".blockNum").text(input_block); // Adds the input to the result message.
+    $(".lotNum").text(input_lot); // Adds the input to the result message.
     
-    // // Finding everything matching the block and lot pair.
-    // // A result greater than 0 means there is a block and lot pair within the database.
-    // if ($.grep(bl_pairs, function(bl_pair, index) {
-    //   return (bl_pair.BLOCK_NUM == input_block && bl_pair.LOT_NUM == input_lot);
-    // }).length > 0) {
-    //    showFound();
-    // }
-    // else {
-    //   showNotFound();
-    // }
+    // Finding everything matching the block and lot pair.
+    // A result greater than 0 means there is a block and lot pair within the database.
+    // Blocks and lots are converted to integers to allow for leading 0s. Radix is 16 to account for letters.
+    if ($.grep(bl_pairs, function(bl_pair, index) {
+      return (parseInt(bl_pair.BLOCK_NUM, 16) === parseInt(input_block, 16) && parseInt(bl_pair.LOT_NUM, 16) === parseInt(input_lot, 16));
+    }).length > 0) {
+       showFound();
+    }
+    else {
+      showNotFound();
+    }
   });
 }
 
@@ -38,8 +37,4 @@ function showNotFound() {
   $("#not-found").attr("tabindex",-1).focus();
 
   $("#found").hide();
-}
-
-function regexInput(input, regex) {
-  regex.exec(input);
 }
