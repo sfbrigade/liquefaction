afb6ba36a9c6532793402fcc16da1bf92f9c6fc9
rcackerman <rebecca@cfa>
2014-01-06 21:54:54 -0500

    Adding regex to check for 0s.

--- checker.js
+++ checker.js
@@ -4,20 +4,21 @@ function checkliquefactionStatus() {
   $.getJSON('./data.json', function(bl_pairs) {
     console.log("loaded JSON");
     var input_block = $('#inputBlock').val();
-    var input_lot = $('#inputLot').val();
-    $(".blockNum").text(input_block); // Adds the input to the result message.
-    $(".lotNum").text(input_lot); // Adds the input to the result message.
+    var input_lot = $('#inputLot').val().match(/[^0].*/);
+    console.log(input_lot[0]);
+    // $(".blockNum").text(input_block); // Adds the input to the result message.
+    // $(".lotNum").text(input_lot); // Adds the input to the result message.
     
-    // Finding everything matching the block and lot pair.
-    // A result greater than 0 means there is a block and lot pair within the database.
-    if ($.grep(bl_pairs, function(bl_pair, index) {
-      return (bl_pair.BLOCK_NUM == input_block && bl_pair.LOT_NUM == input_lot);
-    }).length > 0) {
-       showFound();
-    }
-    else {
-      showNotFound();
-    }
+    // // Finding everything matching the block and lot pair.
+    // // A result greater than 0 means there is a block and lot pair within the database.
+    // if ($.grep(bl_pairs, function(bl_pair, index) {
+    //   return (bl_pair.BLOCK_NUM == input_block && bl_pair.LOT_NUM == input_lot);
+    // }).length > 0) {
+    //    showFound();
+    // }
+    // else {
+    //   showNotFound();
+    // }
   });
 }
 
@@ -37,4 +38,8 @@ function showNotFound() {
   $("#not-found").attr("tabindex",-1).focus();
 
   $("#found").hide();
+}
+
+function regexInput(input, regex) {
+  regex.exec(input);
 }
